8edaa59f6d1512d0d9c71b505d28fd67b784bf48
Pedro <phb.pedroh@gmail.com>
2025-02-19 23:40:05 -0300

    Update exemplo.py

--- exemplo.py
+++ exemplo.py
@@ -10,3 +10,5 @@ if __name__ == "__main__":
     saudacao()
     resultado = soma(5, 3)
     print(f"A soma de 5 e 3 é {resultado}")
+
+    input("Pressione Enter para sair...")  # Adiciona uma pausa para manter a janela aberta
